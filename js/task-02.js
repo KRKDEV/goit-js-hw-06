81ec42f182aa532c1dd4e370c84ba1d32b36094b
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2022-12-12 16:41:11 +0100

    Task 2 - completed

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const listItems = ingredients.forEach((ingredient) => {
+    const createListItem = document.createElement("li");
+    createListItem.textContent = ingredient;
+    createListItem.classList.add("item");
+    list.append(createListItem);
+});
