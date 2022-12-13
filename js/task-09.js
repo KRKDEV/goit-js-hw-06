57fdfdd061df264559b4e3c23ea788355dfc36e4
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2022-12-13 20:40:34 +0100

    Task 9 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const button = document.querySelector("button");
+const currentColor = document.querySelector("span");
+
+button.addEventListener("click", () => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    currentColor.innerHTML = getRandomHexColor();
+});
