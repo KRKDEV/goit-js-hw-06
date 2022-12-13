085b538961270075157a1e6ea17acabae91f2b7a
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2022-12-13 20:09:42 +0100

    Task 8 - completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector("form");
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
 
+    if (email.value === "" || password.value === "") {
+        return window.alert("Please fill in all the fields!");
+        // return console.log("Please fill in all the fields!");
+    }
+    console.log(`Login: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+};
+
+form.addEventListener("submit", handleSubmit);
