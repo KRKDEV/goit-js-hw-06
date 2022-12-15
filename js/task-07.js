981721dcb2d41dcd3d8f108fc2c9e2dc627d1eb4
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2022-12-15 14:54:26 +0100

    Fixed starting value

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,7 @@
 const inputRange = document.querySelector("#font-size-control");
 const fontToChange = document.querySelector("#text");
 
+inputRange.value = inputRange.min;
 inputRange.addEventListener("input", () => {
     fontToChange.style.fontSize = `${inputRange.value}px`;
 });
