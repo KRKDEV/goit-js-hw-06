a58d57b3552175dc1bba7d2b5056586b04011682
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2022-12-15 16:15:56 +0100

    Task 10 - completed

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const boxes = document.getElementById("boxes");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const inputValue = document.querySelector("input");
+let boxSize = 30;
+
+createButton.addEventListener("click", () => {
+    for (let i = 0; i < inputValue.value; i++) {
+        const newBox = document.createElement("div");
+
+        newBox.style.width = `${boxSize}px`;
+        newBox.style.height = `${boxSize}px`;
+        newBox.style.backgroundColor = `${getRandomHexColor()}`;
+        boxSize += 10;
+        boxes.appendChild(newBox);
+    }
+});
+
+destroyButton.addEventListener("click", () => {
+    boxes.innerHTML = "";
+    boxSize = 30;
+});
